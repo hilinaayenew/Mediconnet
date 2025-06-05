c1690cf82692309c16eb1dd5e857433c2b38e59c
Hilina ayenew <hilinaayenew47@gmail.com>
2025-06-06 02:13:21 +0300

    updated

--- Mediconnet-Backend/middleware/centerAuthMiddleware.js
+++ Mediconnet-Backend/middleware/centerAuthMiddleware.js
@@ -1,29 +1,24 @@
-const mongoose = require('mongoose');
 const Hospital = require('../models/hospital');
 
 const authenticateHospital = async (req, res, next) => {
   try {
-    const secretKey = req.headers['x-api-key']?.trim();
-    const hospitalID = req.headers['hospitalid']?.trim(); 
-
+    const secretKey = req.headers['x-api-key'];
+    const hospitalID = req.headers['hospitalid']; 
     if (!secretKey || !hospitalID) {
       return res.status(401).json({ error: 'API key and hospital ID are required' });
     }
-    consolde.log(hospitalID)
 
-    if (!mongoose.Types.ObjectId.isValid(hospitalID)) {
-      return res.status(400).json({ error: 'Invalid hospital ID format' });
-    }
+    console.log('Hospital ID:', hospitalID);
 
     const hospital = await Hospital.findById(hospitalID);
-    console.log(hospital)
-    if (!hospital) {
-      consolde.log(hospitalID)
-      return res.status(404).json({ error: `Hospital not found` });
-    }
+    console.log('Hospital:', hospital);
 
-    if (secretKey !== hospital.secretKey) {
-      return res.status(401).json({ error: 'Invalid API key or hospital not approved' });
+    if (!hospital) {
+      return res.status(404).json({ error: 'Hospital not found' });
+    }
+console.log('Hospital found:', hospital.secreteKey);
+    if (secretKey !== hospital.secreteKey ) {
+      return res.status(403).json({ error: 'Invalid API key or hospital not approved' });
     }
 
     req.hospital = hospital;
